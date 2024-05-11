25c9f2eb91ef67428f654f8ab016e650dad6e04e
Burak Kaan Köse <bkaankose@outlook.com>
2024-05-12 01:54:21 +0200

    New empty folder request

---
+++ Wino.Core/Requests/EmptyFolderRequest.cs
@@ -0,0 +1,13 @@
+﻿using Wino.Core.Domain.Entities;
+using Wino.Core.Domain.Enums;
+using Wino.Core.Domain.Models.Requests;
+
+namespace Wino.Core.Requests
+{
+    public record EmptyFolderRequest(MailItemFolder Folder) : FolderRequestBase(Folder, MailSynchronizerOperation.EmptyFolder)
+    {
+        public override void ApplyUIChanges() { }
+
+        public override void RevertUIChanges() { }
+    }
+}
