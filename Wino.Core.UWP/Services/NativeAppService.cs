3d69f96b9675afbee31baca612077e7a0ee9bbaf
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-07-01 01:28:18 +0200

    Removed quill subfolder

--- Wino.Core.UWP/Services/NativeAppService.cs
+++ Wino.Core.UWP/Services/NativeAppService.cs
@@ -81,7 +81,7 @@ namespace Wino.Services
         {
             if (string.IsNullOrEmpty(_editorBundlePath))
             {
-                var editorFileFromBundle = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///JS/Quill/editor.html"))
+                var editorFileFromBundle = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///JS/editor.html"))
                     .AsTask()
                     .ConfigureAwait(false);
 
