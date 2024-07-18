95181ba57e14f563379a7111caa5fee4d2bca7ee
Aleh Khantsevich <Aleh_Khantsevich@epam.com>
2024-07-18 19:47:28 +0200

    Remove unused methods

--- Wino.Core/Services/FontService.cs
+++ Wino.Core/Services/FontService.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Serilog;
 using SkiaSharp;
 using Wino.Core.Domain.Interfaces;
 
 namespace Wino.Core.Services;
 
-public class FontService(IPreferencesService preferencesService) : IFontService
+public class FontService() : IFontService
 {
-    private readonly IPreferencesService _preferencesService = preferencesService;
-    private readonly ILogger _logger = Log.ForContext<FontService>();
-
     private static readonly Lazy<List<string>> _availableFonts = new(InitializeFonts);
     private static readonly List<string> _defaultFonts = ["Arial", "Calibri", "Trebuchet MS", "Tahoma", "Verdana", "Courier New", "Georgia", "Times New Roman"];
-
-    private const string FallbackFont = "Arial";
 
     private static List<string> InitializeFonts()
     {
@@ -29,38 +23,4 @@ public class FontService(IPreferencesService preferencesService) : IFontService
     }
 
     public List<string> GetFonts() => _availableFonts.Value;
-
-    public string GetCurrentReaderFont() => _availableFonts.Value.Find(f => f == _preferencesService.ReaderFont) ?? FallbackFont;
-    public int GetCurrentReaderFontSize() => _preferencesService.ReaderFontSize;
-
-    public void SetReaderFont(string font)
-    {
-        _preferencesService.ReaderFont = font;
-
-        _logger.Information("Default reader font is changed to {Font}", font);
-    }
-
-    public void SetReaderFontSize(int size)
-    {
-        _preferencesService.ReaderFontSize = size;
-
-        _logger.Information("Default reader font size is changed to {Size}", size);
-    }
-
-    public string GetCurrentComposerFont() => _availableFonts.Value.Find(f => f == _preferencesService.ComposerFont) ?? FallbackFont;
-    public int GetCurrentComposerFontSize() => _preferencesService.ComposerFontSize;
-
-    public void SetComposerFont(string font)
-    {
-        _preferencesService.ComposerFont = font;
-
-        _logger.Information("Default composer font is changed to {Font}", font);
-    }
-
-    public void SetComposerFontSize(int size)
-    {
-        _preferencesService.ComposerFontSize = size;
-
-        _logger.Information("Default composer font size is changed to {Size}", size);
-    }
 }
