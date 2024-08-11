169c2146d989b3bc10c59f47485736f7b8361469
Burak Kaan Köse <bkaankose@outlook.com>
2024-08-11 22:41:31 +0200

    Fix crashes for signatures without images.

--- Wino.Core/Extensions/MimeExtensions.cs
+++ Wino.Core/Extensions/MimeExtensions.cs
@@ -66,44 +66,50 @@ namespace Wino.Core.Extensions
             var doc = new HtmlDocument();
             doc.LoadHtml(htmlContent);
 
-            foreach (var node in doc.DocumentNode.SelectNodes("//img"))
+            var imgNodes = doc.DocumentNode.SelectNodes("//img");
+
+            if (imgNodes != null)
             {
-                var src = node.GetAttributeValue("src", string.Empty);
-
-                if (string.IsNullOrEmpty(src)) continue;
-
-                if (!src.StartsWith("data:image"))
+                foreach (var node in imgNodes)
                 {
-                    continue;
+                    var src = node.GetAttributeValue("src", string.Empty);
+
+                    if (string.IsNullOrEmpty(src)) continue;
+
+                    if (!src.StartsWith("data:image"))
+                    {
+                        continue;
+                    }
+
+                    var parts = src.Substring(11).Split([";base64,"], StringSplitOptions.None);
+
+                    string mimeType = parts[0];
+                    string base64Content = parts[1];
+
+                    var alt = node.GetAttributeValue("alt", $"Embedded_Image.{mimeType}");
+
+                    // Convert the base64 content to binary data
+                    byte[] imageData = Convert.FromBase64String(base64Content);
+
+                    // Create a new linked resource as MimePart
+                    var image = new MimePart("image", mimeType)
+                    {
+                        ContentId = MimeUtils.GenerateMessageId(),
+                        Content = new MimeContent(new MemoryStream(imageData)),
+                        ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
+                        ContentDescription = alt.Replace(" ", "_"),
+                        FileName = alt,
+                        ContentTransferEncoding = ContentEncoding.Base64
+                    };
+
+                    bodyBuilder.LinkedResources.Add(image);
+
+                    node.SetAttributeValue("src", $"cid:{image.ContentId}");
                 }
-
-                var parts = src.Substring(11).Split([";base64,"], StringSplitOptions.None);
-
-                string mimeType = parts[0];
-                string base64Content = parts[1];
-
-                var alt = node.GetAttributeValue("alt", $"Embedded_Image.{mimeType}");
-
-                // Convert the base64 content to binary data
-                byte[] imageData = Convert.FromBase64String(base64Content);
-
-                // Create a new linked resource as MimePart
-                var image = new MimePart("image", mimeType)
-                {
-                    ContentId = MimeUtils.GenerateMessageId(),
-                    Content = new MimeContent(new MemoryStream(imageData)),
-                    ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
-                    ContentDescription = alt.Replace(" ", "_"),
-                    FileName = alt,
-                    ContentTransferEncoding = ContentEncoding.Base64
-                };
-
-                bodyBuilder.LinkedResources.Add(image);
-
-                node.SetAttributeValue("src", $"cid:{image.ContentId}");
             }
 
             bodyBuilder.HtmlBody = doc.DocumentNode.InnerHtml;
+
             if (!string.IsNullOrEmpty(bodyBuilder.HtmlBody))
             {
                 bodyBuilder.TextBody = HtmlAgilityPackExtensions.GetPreviewText(bodyBuilder.HtmlBody);
