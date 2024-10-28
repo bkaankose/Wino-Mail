f5e7dbccb0c2346a86290a153305a4b652045bc1
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-10-28 20:36:07 +0100

    Removed margin from mail list Adjusted margin for group header to match new mail list

--- Wino.Mail/Controls/Advanced/WinoListView.cs
+++ Wino.Mail/Controls/Advanced/WinoListView.cs
@@ -406,20 +406,5 @@ namespace Wino.Controls.Advanced
                 internalScrollviewer.ViewChanged -= InternalScrollVeiwerViewChanged;
             }
         }
-
-        protected override DependencyObject GetContainerForItemOverride()
-        {
-            var itemContainer = base.GetContainerForItemOverride();
-
-            // Adjust scrolling margin for all containers.
-            // I don't want to override the default style for this.
-
-            if (itemContainer is ListViewItem listViewItem)
-            {
-                listViewItem.Margin = new Thickness(0, 0, 12, 4);
-            }
-
-            return itemContainer;
-        }
     }
 }
