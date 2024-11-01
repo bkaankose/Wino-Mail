bc9307c8295f93ce1dc728e64ec6525992b3baa1
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2024-11-01 19:12:47 +0100

    Added summary

--- Wino.Core.Domain/Translator.cs
+++ Wino.Core.Domain/Translator.cs
@@ -2,5 +2,9 @@
 
 namespace Wino.Core.Domain;
 
+/// <summary>
+/// Translator class for translation.
+/// All translations generated automatically by the source generator.
+/// </summary>
 [TranslatorGen]
 public partial class Translator;
