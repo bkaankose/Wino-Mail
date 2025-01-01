215dc6ea6d5982c083e6a950a6fb341917a78d09
Burak Kaan Köse <bkaankose@outlook.com>
2025-01-01 20:19:31 +0100

    Basic dragging state for calendar control

--- Wino.Calendar/Controls/CalendarItemControl.xaml.cs
+++ Wino.Calendar/Controls/CalendarItemControl.xaml.cs
@@ -7,13 +7,20 @@ namespace Wino.Calendar.Controls
 {
     public sealed partial class CalendarItemControl : UserControl
     {
+        public static readonly DependencyProperty CalendarItemProperty = DependencyProperty.Register(nameof(CalendarItem), typeof(ICalendarItem), typeof(CalendarItemControl), new PropertyMetadata(null, new PropertyChangedCallback(OnCalendarItemChanged)));
+        public static readonly DependencyProperty IsDraggingProperty = DependencyProperty.Register(nameof(IsDragging), typeof(bool), typeof(CalendarItemControl), new PropertyMetadata(false));
+
         public ICalendarItem CalendarItem
         {
             get { return (CalendarItemViewModel)GetValue(CalendarItemProperty); }
             set { SetValue(CalendarItemProperty, value); }
         }
 
-        public static readonly DependencyProperty CalendarItemProperty = DependencyProperty.Register(nameof(CalendarItem), typeof(ICalendarItem), typeof(CalendarItemControl), new PropertyMetadata(null, new PropertyChangedCallback(OnCalendarItemChanged)));
+        public bool IsDragging
+        {
+            get { return (bool)GetValue(IsDraggingProperty); }
+            set { SetValue(IsDraggingProperty, value); }
+        }
 
         public CalendarItemControl()
         {
@@ -45,5 +52,9 @@ namespace Wino.Calendar.Controls
                 VisualStateManager.GoToState(this, "RegularEvent", true);
             }
         }
+
+        private void ControlDragStarting(UIElement sender, DragStartingEventArgs args) => IsDragging = true;
+
+        private void ControlDropped(UIElement sender, DropCompletedEventArgs args) => IsDragging = false;
     }
 }
