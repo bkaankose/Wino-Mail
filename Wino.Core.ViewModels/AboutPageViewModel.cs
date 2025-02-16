190939293b32a62507ebf33c5b039aad330346b4
Aleh Khantsevich <aleh.khantsevich@gmail.com>
2025-02-16 14:14:57 +0100

    fix namespaces again

--- Wino.Core.ViewModels/AboutPageViewModel.cs
+++ Wino.Core.ViewModels/AboutPageViewModel.cs
@@ -6,125 +6,124 @@ using Wino.Core.Domain;
 using Wino.Core.Domain.Enums;
 using Wino.Core.Domain.Interfaces;
 
-namespace Wino.Core.ViewModels
+namespace Wino.Core.ViewModels;
+
+public partial class AboutPageViewModel : CoreBaseViewModel
 {
-    public partial class AboutPageViewModel : CoreBaseViewModel
+    private readonly IStoreRatingService _storeRatingService;
+    private readonly IMailDialogService _dialogService;
+    private readonly INativeAppService _nativeAppService;
+    private readonly IApplicationConfiguration _appInitializerService;
+    private readonly IClipboardService _clipboardService;
+    private readonly IFileService _fileService;
+    private readonly IWinoLogger _logInitializer;
+
+    public string VersionName => _nativeAppService.GetFullAppVersion();
+    public string DiscordChannelUrl => "https://discord.gg/windows-apps-hub-714581497222398064";
+    public string GitHubUrl => "https://github.com/bkaankose/Wino-Mail/";
+    public string PrivacyPolicyUrl => "https://www.winomail.app/support/privacy";
+    public string PaypalUrl => "https://paypal.me/bkaankose?country.x=PL&locale.x=en_US";
+
+    public IPreferencesService PreferencesService { get; }
+
+    public AboutPageViewModel(IStoreRatingService storeRatingService,
+                              IMailDialogService dialogService,
+                              INativeAppService nativeAppService,
+                              IPreferencesService preferencesService,
+                              IApplicationConfiguration appInitializerService,
+                              IClipboardService clipboardService,
+                              IFileService fileService,
+                              IWinoLogger logInitializer)
     {
-        private readonly IStoreRatingService _storeRatingService;
-        private readonly IMailDialogService _dialogService;
-        private readonly INativeAppService _nativeAppService;
-        private readonly IApplicationConfiguration _appInitializerService;
-        private readonly IClipboardService _clipboardService;
-        private readonly IFileService _fileService;
-        private readonly IWinoLogger _logInitializer;
+        _storeRatingService = storeRatingService;
+        _dialogService = dialogService;
+        _nativeAppService = nativeAppService;
+        _logInitializer = logInitializer;
+        _appInitializerService = appInitializerService;
+        _clipboardService = clipboardService;
+        _fileService = fileService;
 
-        public string VersionName => _nativeAppService.GetFullAppVersion();
-        public string DiscordChannelUrl => "https://discord.gg/windows-apps-hub-714581497222398064";
-        public string GitHubUrl => "https://github.com/bkaankose/Wino-Mail/";
-        public string PrivacyPolicyUrl => "https://www.winomail.app/support/privacy";
-        public string PaypalUrl => "https://paypal.me/bkaankose?country.x=PL&locale.x=en_US";
+        PreferencesService = preferencesService;
+    }
 
-        public IPreferencesService PreferencesService { get; }
+    protected override void OnActivated()
+    {
+        base.OnActivated();
 
-        public AboutPageViewModel(IStoreRatingService storeRatingService,
-                                  IMailDialogService dialogService,
-                                  INativeAppService nativeAppService,
-                                  IPreferencesService preferencesService,
-                                  IApplicationConfiguration appInitializerService,
-                                  IClipboardService clipboardService,
-                                  IFileService fileService,
-                                  IWinoLogger logInitializer)
+        PreferencesService.PreferenceChanged -= PreferencesChanged;
+        PreferencesService.PreferenceChanged += PreferencesChanged;
+    }
+
+    protected override void OnDeactivated()
+    {
+        base.OnDeactivated();
+
+        PreferencesService.PreferenceChanged -= PreferencesChanged;
+    }
+
+    private void PreferencesChanged(object sender, string e)
+    {
+        if (e == nameof(PreferencesService.IsLoggingEnabled))
         {
-            _storeRatingService = storeRatingService;
-            _dialogService = dialogService;
-            _nativeAppService = nativeAppService;
-            _logInitializer = logInitializer;
-            _appInitializerService = appInitializerService;
-            _clipboardService = clipboardService;
-            _fileService = fileService;
-
-            PreferencesService = preferencesService;
+            _logInitializer.RefreshLoggingLevel();
         }
+    }
 
-        protected override void OnActivated()
+    [RelayCommand]
+    private async Task CopyDiagnosticId()
+    {
+        try
         {
-            base.OnActivated();
-
-            PreferencesService.PreferenceChanged -= PreferencesChanged;
-            PreferencesService.PreferenceChanged += PreferencesChanged;
+            await _clipboardService.CopyClipboardAsync(PreferencesService.DiagnosticId);
+            _dialogService.InfoBarMessage(Translator.Buttons_Copy, string.Format(Translator.ClipboardTextCopied_Message, "Id"), InfoBarMessageType.Success);
         }
-
-        protected override void OnDeactivated()
+        catch (Exception ex)
         {
-            base.OnDeactivated();
-
-            PreferencesService.PreferenceChanged -= PreferencesChanged;
+            _dialogService.InfoBarMessage(Translator.GeneralTitle_Error, string.Format(Translator.ClipboardTextCopyFailed_Message, "Id"), InfoBarMessageType.Error);
+            Log.Error(ex, "Failed to copy diagnostic id to clipboard.");
         }
+    }
 
-        private void PreferencesChanged(object sender, string e)
+    [RelayCommand]
+    private async Task ShareWinoLogAsync()
+    {
+        var appDataFolder = _appInitializerService.ApplicationDataFolderPath;
+
+        var selectedFolderPath = await _dialogService.PickWindowsFolderAsync();
+
+        if (string.IsNullOrEmpty(selectedFolderPath)) return;
+
+        var areLogsSaved = await _fileService.SaveLogsToFolderAsync(appDataFolder, selectedFolderPath).ConfigureAwait(false);
+
+        if (areLogsSaved)
         {
-            if (e == nameof(PreferencesService.IsLoggingEnabled))
-            {
-                _logInitializer.RefreshLoggingLevel();
-            }
+            _dialogService.InfoBarMessage(Translator.Info_LogsSavedTitle, string.Format(Translator.Info_LogsSavedMessage, Constants.LogArchiveFileName), InfoBarMessageType.Success);
         }
-
-        [RelayCommand]
-        private async Task CopyDiagnosticId()
+        else
         {
-            try
-            {
-                await _clipboardService.CopyClipboardAsync(PreferencesService.DiagnosticId);
-                _dialogService.InfoBarMessage(Translator.Buttons_Copy, string.Format(Translator.ClipboardTextCopied_Message, "Id"), InfoBarMessageType.Success);
-            }
-            catch (Exception ex)
-            {
-                _dialogService.InfoBarMessage(Translator.GeneralTitle_Error, string.Format(Translator.ClipboardTextCopyFailed_Message, "Id"), InfoBarMessageType.Error);
-                Log.Error(ex, "Failed to copy diagnostic id to clipboard.");
-            }
+            _dialogService.InfoBarMessage(Translator.Info_LogsNotFoundTitle, Translator.Info_LogsNotFoundMessage, InfoBarMessageType.Error);
         }
+    }
 
-        [RelayCommand]
-        private async Task ShareWinoLogAsync()
+    [RelayCommand]
+    private async Task Navigate(object url)
+    {
+        if (url is string stringUrl)
         {
-            var appDataFolder = _appInitializerService.ApplicationDataFolderPath;
-
-            var selectedFolderPath = await _dialogService.PickWindowsFolderAsync();
-
-            if (string.IsNullOrEmpty(selectedFolderPath)) return;
-
-            var areLogsSaved = await _fileService.SaveLogsToFolderAsync(appDataFolder, selectedFolderPath).ConfigureAwait(false);
-
-            if (areLogsSaved)
-            {
-                _dialogService.InfoBarMessage(Translator.Info_LogsSavedTitle, string.Format(Translator.Info_LogsSavedMessage, Constants.LogArchiveFileName), InfoBarMessageType.Success);
-            }
+            if (stringUrl == "Store")
+                await ShowRateDialogAsync();
             else
             {
-                _dialogService.InfoBarMessage(Translator.Info_LogsNotFoundTitle, Translator.Info_LogsNotFoundMessage, InfoBarMessageType.Error);
+                // Discord disclaimer message about server.
+                if (stringUrl == DiscordChannelUrl)
+                    await _dialogService.ShowMessageAsync(Translator.DiscordChannelDisclaimerMessage,
+                                                         Translator.DiscordChannelDisclaimerTitle,
+                                                         WinoCustomMessageDialogIcon.Warning);
+
+                await _nativeAppService.LaunchUriAsync(new Uri(stringUrl));
             }
         }
-
-        [RelayCommand]
-        private async Task Navigate(object url)
-        {
-            if (url is string stringUrl)
-            {
-                if (stringUrl == "Store")
-                    await ShowRateDialogAsync();
-                else
-                {
-                    // Discord disclaimer message about server.
-                    if (stringUrl == DiscordChannelUrl)
-                        await _dialogService.ShowMessageAsync(Translator.DiscordChannelDisclaimerMessage,
-                                                             Translator.DiscordChannelDisclaimerTitle,
-                                                             WinoCustomMessageDialogIcon.Warning);
-
-                    await _nativeAppService.LaunchUriAsync(new Uri(stringUrl));
-                }
-            }
-        }
-
-        private Task ShowRateDialogAsync() => _storeRatingService.LaunchStorePageForReviewAsync();
     }
+
+    private Task ShowRateDialogAsync() => _storeRatingService.LaunchStorePageForReviewAsync();
 }
--- Wino.Services/ServicesContainerSetup.cs
+++ Wino.Services/ServicesContainerSetup.cs
@@ -2,35 +2,34 @@
 using Wino.Core.Domain.Interfaces;
 using Wino.Services.Threading;
 
-namespace Wino.Services
+namespace Wino.Services;
+
+public static class ServicesContainerSetup
 {
-    public static class ServicesContainerSetup
+    public static void RegisterSharedServices(this IServiceCollection services)
     {
-        public static void RegisterSharedServices(this IServiceCollection services)
-        {
-            services.AddSingleton<ITranslationService, TranslationService>();
-            services.AddSingleton<IDatabaseService, DatabaseService>();
+        services.AddSingleton<ITranslationService, TranslationService>();
+        services.AddSingleton<IDatabaseService, DatabaseService>();
 
-            services.AddSingleton<IApplicationConfiguration, ApplicationConfiguration>();
-            services.AddSingleton<IWinoLogger, WinoLogger>();
-            services.AddSingleton<ILaunchProtocolService, LaunchProtocolService>();
-            services.AddSingleton<IMimeFileService, MimeFileService>();
+        services.AddSingleton<IApplicationConfiguration, ApplicationConfiguration>();
+        services.AddSingleton<IWinoLogger, WinoLogger>();
+        services.AddSingleton<ILaunchProtocolService, LaunchProtocolService>();
+        services.AddSingleton<IMimeFileService, MimeFileService>();
 
-            services.AddTransient<ICalendarService, CalendarService>();
-            services.AddTransient<IMailService, MailService>();
-            services.AddTransient<IFolderService, FolderService>();
-            services.AddTransient<IAccountService, AccountService>();
-            services.AddTransient<IContactService, ContactService>();
-            services.AddTransient<ISignatureService, SignatureService>();
-            services.AddTransient<IContextMenuItemService, ContextMenuItemService>();
-            services.AddTransient<ISpecialImapProviderConfigResolver, SpecialImapProviderConfigResolver>();
+        services.AddTransient<ICalendarService, CalendarService>();
+        services.AddTransient<IMailService, MailService>();
+        services.AddTransient<IFolderService, FolderService>();
+        services.AddTransient<IAccountService, AccountService>();
+        services.AddTransient<IContactService, ContactService>();
+        services.AddTransient<ISignatureService, SignatureService>();
+        services.AddTransient<IContextMenuItemService, ContextMenuItemService>();
+        services.AddTransient<ISpecialImapProviderConfigResolver, SpecialImapProviderConfigResolver>();
 
-            services.AddSingleton<IThreadingStrategyProvider, ThreadingStrategyProvider>();
-            services.AddTransient<IOutlookThreadingStrategy, OutlookThreadingStrategy>();
-            services.AddTransient<IGmailThreadingStrategy, GmailThreadingStrategy>();
-            services.AddTransient<IImapThreadingStrategy, ImapThreadingStrategy>();
+        services.AddSingleton<IThreadingStrategyProvider, ThreadingStrategyProvider>();
+        services.AddTransient<IOutlookThreadingStrategy, OutlookThreadingStrategy>();
+        services.AddTransient<IGmailThreadingStrategy, GmailThreadingStrategy>();
+        services.AddTransient<IImapThreadingStrategy, ImapThreadingStrategy>();
 
 
-        }
     }
 }
