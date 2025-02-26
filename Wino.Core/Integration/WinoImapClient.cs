b6821746d0adf9bb9cf5bf942a4b209c6ebf1cad
Burak Kaan Köse <bkaankose@outlook.com>
2025-02-26 23:11:49 +0100

    Locked busy scope to handle disconnections properly.

--- Wino.Core/Integration/WinoImapClient.cs
+++ Wino.Core/Integration/WinoImapClient.cs
@@ -1,4 +1,6 @@
-﻿using MailKit;
+﻿using System;
+using System.Threading;
+using MailKit;
 using MailKit.Net.Imap;
 using Serilog;
 
@@ -9,6 +11,8 @@ namespace Wino.Core.Integration;
 /// </summary>
 internal class WinoImapClient : ImapClient
 {
+    private int _busyCount;
+
     /// <summary>
     /// Gets or internally sets whether the QRESYNC extension is enabled.
     /// It is set by ImapClientPool immidiately after the authentication.
@@ -44,6 +48,34 @@ internal class WinoImapClient : ImapClient
         else
         {
             Log.Debug("Imap client connection is dropped by server.");
+        }
+    }
+
+    public bool IsBusy() => _busyCount > 0;
+
+    public IDisposable GetBusyScope()
+    {
+        Interlocked.Increment(ref _busyCount);
+        return new BusyScope(this);
+    }
+
+    private class BusyScope : IDisposable
+    {
+        private readonly WinoImapClient _client;
+        private bool _disposed;
+
+        public BusyScope(WinoImapClient client)
+        {
+            _client = client;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Interlocked.Decrement(ref _client._busyCount);
+                _disposed = true;
+            }
         }
     }
 
