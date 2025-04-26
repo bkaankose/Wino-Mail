f37a51b46fab94c0131f28e562c9aed3238c3731
Burak Kaan Köse <bkaankose@outlook.com>
2025-04-26 10:51:14 +0200

    Remove test code.

--- Wino.Server/MessageHandlers/MailSynchronizationRequestHandler.cs
+++ Wino.Server/MessageHandlers/MailSynchronizationRequestHandler.cs
@@ -51,14 +51,6 @@ public class MailSynchronizationRequestHandler : ServerMessageHandler<NewMailSyn
 
         try
         {
-            // test
-            //if (synchronizer.Account.ProviderType == MailProviderType.Gmail)
-            //{
-
-            //    await synchronizer.OnlineSearchAsync("label:unread", null, cancellationToken);
-            //    return WinoServerResponse<MailSynchronizationResult>.CreateSuccessResponse(MailSynchronizationResult.Canceled);
-            //}
-
             var synchronizationResult = await synchronizer.SynchronizeMailsAsync(message.Options, cancellationToken).ConfigureAwait(false);
 
             if (synchronizationResult.DownloadedMessages?.Any() ?? false || !synchronizer.Account.Preferences.IsNotificationsEnabled)
