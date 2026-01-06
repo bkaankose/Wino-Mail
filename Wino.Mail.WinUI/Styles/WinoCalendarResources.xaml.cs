be6b23c47b2b20493ff6120a95ac8d45b8886692
Burak Kaan Köse <bkaankose@outlook.com>
2026-01-06 11:45:03 +0100

    AOT safe panels.

--- Wino.Mail.WinUI/Styles/WinoCalendarResources.xaml.cs
+++ Wino.Mail.WinUI/Styles/WinoCalendarResources.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.UI.Xaml;
+﻿using CommunityToolkit.WinUI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Wino.Calendar.Controls;
+using Wino.Core.Domain.Models.Calendar;
 
 namespace Wino.Styles;
 
@@ -7,5 +11,39 @@ public sealed partial class WinoCalendarResources : ResourceDictionary
     public WinoCalendarResources()
     {
         this.InitializeComponent();
+    }
+
+    private void OnRegularEventItemsControlLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is ItemsControl itemsControl && itemsControl.DataContext is CalendarDayModel dayModel)
+        {
+            if (itemsControl.ItemsPanelRoot is WinoCalendarPanel panel)
+            {
+                panel.HourHeight = dayModel.CalendarRenderOptions.CalendarSettings.HourHeight;
+                panel.Period = dayModel.Period;
+            }
+        }
+    }
+
+    private void OnDayColumnsItemsControlLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is ItemsControl itemsControl && itemsControl.DataContext is DayRangeRenderModel rangeModel)
+        {
+            if (itemsControl.ItemsPanelRoot is UniformGrid uniformGrid)
+            {
+                uniformGrid.Columns = rangeModel.CalendarRenderOptions.TotalDayCount;
+            }
+        }
+    }
+
+    private void OnEventGridsItemsControlLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is ItemsControl itemsControl && itemsControl.DataContext is DayRangeRenderModel rangeModel)
+        {
+            if (itemsControl.ItemsPanelRoot is UniformGrid uniformGrid)
+            {
+                uniformGrid.Columns = rangeModel.CalendarRenderOptions.TotalDayCount;
+            }
+        }
     }
 }
